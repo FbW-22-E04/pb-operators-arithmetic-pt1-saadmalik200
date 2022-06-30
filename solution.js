62997641e98e313ea0b7c4190dd53e596ab43a22
Saad Malik <saad_malik200@hotmail.com>
2022-06-30 14:22:38 +0200

    exercise 2

--- solution.js
+++ solution.js
@@ -1,1 +1,36 @@
 // write your code in this file
+console.log(`-------- Basic Arithmetic Problems --------`);
+console.log(`Q1: ${6450 / 15} tickets are sold`);
+console.log(`Q2: Sylvia makes ${500 * 52} dollar per year`);
+console.log(`-------- Percentage --------`);
+console.log(`Q3: The percentage is: ${(17 / 30) * 100} %`);
+console.log(`-------- Geometry Formulas --------`);
+console.log(`Q4: Perimeter of square is: ${4.75 * 4}cm`);
+console.log(`Q5: Perimeter of triangle is: ${5 + 6 + 7}cm`);
+console.log(`Q6: Area of a square is: ${5 * 5}`);
+console.log(`Q7: Area of a right angle triangle: ${(3 * 4) / 2}`);
+console.log(`Q8" Volume of cube is: ${9 * 9 * 9}`);
+console.log(`-------- Consumer Formula --------`);
+const bill1 = 22.35 + 0.1 * 22.35;
+const bill2 = 26.67 + 0.15 * 22.35;
+const bill3 = 35.92 + 0.2 * 22.35;
+console.log(
+  `Q9: The total for bill 1 is: ${bill1}, The total for bill 2 is: ${bill2}, The total for bill 3 is: ${bill3}`
+);
+console.log(`-------- Average --------`);
+console.log(
+  `Q10: Noemy's average hours worked per day are: ${
+    (8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 + 7 + 4) / 10
+  }`
+);
+console.log(
+  `Q11: Score in the sixth test: ${85 * 6 - (75 + 70 + 85 + 90 + 100)}`
+);
+
+const scoreRequired = 80 * 9;
+const currentScore = 78 * 8;
+console.log(
+  `Q12: James needs a minimum of ${
+    scoreRequired - currentScore
+  }% to get an 80% average`
+);
